7681fddc1f06da72a7264e477b0ec699f1304660
孟宪亮 <meng.xianliang@immomo.com>
2017-03-08 19:45:52 +0800

    push origin

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var loaderUtils = require("loader-utils")
+
 module.exports = function (source,options) {
-	console.log(this.options)
+	console.log(this.loaderUtils.getOptions(this))
+    // loader query string
+    
 	source = source.replace(/([0-9]*\.[0-9]+|[0-9]+)px/g, function (number) {
 		return number.slice(0, -2)
 	})

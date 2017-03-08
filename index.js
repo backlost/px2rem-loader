b157351e72b204132e29033aaae46301519ba955
孟宪亮 <meng.xianliang@immomo.com>
2017-03-08 19:55:57 +0800

    add options

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var loaderUtils = require('loader-utils')
+const loaderUtils = require('loader-utils')
 
 module.exports = function (source, options) {
-    var baseFont = loaderUtils.getOptions(this)
+    const options = loaderUtils.getOptions(this)||{}
     // loader query string
-
+	const baseFont  =parseInt(options.baseSize||36)
     source = source.replace(/([0-9]*\.[0-9]+|[0-9]+)px/g, function (number) {
         return `${number.slice(0, -2) / baseFont}rem`
     })

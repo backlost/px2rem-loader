e7215d36ca2d23b0f9d3ba886b61bb2c5329814d
孟宪亮 <meng.xianliang@immomo.com>
2017-03-08 19:51:51 +0800

    add loader

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var loaderUtils = require("loader-utils")
+var loaderUtils = require('loader-utils')
 
-module.exports = function (source,options) {
-	console.log(loaderUtils.getOptions(this))
+module.exports = function (source, options) {
+    var baseFont = loaderUtils.getOptions(this)
     // loader query string
-    
-	source = source.replace(/([0-9]*\.[0-9]+|[0-9]+)px/g, function (number) {
-		return number.slice(0, -2)
-	})
-	return source;
-};
+
+    source = source.replace(/([0-9]*\.[0-9]+|[0-9]+)px/g, function (number) {
+        return `${number.slice(0, -2) / baseFont}rem`
+    })
+    return source
+}

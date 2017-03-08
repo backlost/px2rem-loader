24e8394beea164c815de51d73a7c95c7966f25e2
孟宪亮 <meng.xianliang@immomo.com>
2017-03-08 19:30:03 +0800

    cout option

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function (source,options) {
-	console.log(options)
+	console.log(this.options)
 	source = source.replace(/([0-9]*\.[0-9]+|[0-9]+)px/g, function (number) {
 		return number.slice(0, -2)
 	})

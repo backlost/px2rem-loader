cf3ff12db91dbf0d7330637a0ae9e05e0969ec80
孟宪亮 <meng.xianliang@immomo.com>
2017-03-08 19:25:50 +0800

    delete some

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = function (source,options) {
-	console.log(source)
 	console.log(options)
 	source = source.replace(/([0-9]*\.[0-9]+|[0-9]+)px/g, function (number) {
 		return number.slice(0, -2)
